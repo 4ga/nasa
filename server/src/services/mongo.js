d768f2d465145ab479b166e39b4da48acde79580
Grace Akpan <opps4lrng@gmail.com>
2025-08-13 20:17:22 -0600

    Update to workflow and mongoDB services

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const MONGO_URL = process.env.MONGODB_URL;
+const MONGO_URL = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/nasa';
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
